085cd4dae4179bc9aaa80001a095df97329bf814
NikUlj <nik127.uljarevic@gmail.com>
2024-12-27 15:40:09 +0100

    Added simple AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int health = 20;
+    private Transform _playerTransform;
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        
+        if (player != null)
+        {
+            _playerTransform = player.transform;
+        }
+        else
+        {
+            Debug.LogWarning("Player transform not found");
+        }
+    }
+
+    private void Update()
+    {
+        _agent.destination = _playerTransform.position;
+    }
 
     public void TakeDamage(int damage)
     {
